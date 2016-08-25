0158a518090a9f63653aad15b2eee2df06a0bf82
John Otander <johnotander@gmail.com>
2016-08-24 23:46:15 -0600

    Begin featured section

--- pages/home.js
+++ pages/home.js
@@ -11,6 +11,21 @@ module.exports = (state, prev, send) => html`
 		<div class='bg-washed-blue'>
 			<div class='pa2 mw8 center'>
 				<h3 class='f5 ttu tracked normal'>Featured</h3>
+				<div class='cf'>
+					<div class='fl w-60 ba b--light-gray br2 bg-gray'>
+            <div class=''>
+              <div class='cf white'>
+                <div class='tr'>
+                  <p class='f1 f-subheadline-m f-subheadline-l b mt0 pa2'>3"</p>
+                </div>
+              </div>
+              <div class='bg-white pa3 br2 br--bottom'>
+                <p class='f5 b mv0'>Bridger Bowl</p>
+                <p class='f6 mid-gray mv0'>Bozeman, MT</p>
+              </div>
+            </div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<footer class='bt b--light-gray bg-black-05 pa3 pa6-m pa6-l tc mid-gray'>
