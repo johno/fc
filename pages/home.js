ed2f58644357c30df818eeccaf266bf4a55cb704
John Otander <johnotander@gmail.com>
2016-08-24 22:56:12 -0600

    Begin working on the homepage

--- pages/home.js
+++ pages/home.js
@@ -2,12 +2,19 @@ const html = require('choo/html')
 
 module.exports = (state, prev, send) => html`
   <main>
-    <h1>Hello, World!</h1>
-    <p>If you are seeing this, then the generator works!</p>
-    <h2>Demo</h2>
-    <h3>${state.title}</h3>
-    <input
-      type="text"
-      oninput=${(e) => send('update', { value: e.target.value })} />
+    <link rel='stylesheet' href='https://npmcdn.com/tachyons@4.1.1/css/tachyons.min.css'/>
+		<header class='tc pv2 pv4-m pv4-l bb b--light-gray'>
+			<span class='f-subheadline mid-gray'>≙</span>
+			<h1 class='f3 mid-gray'>First Chair</h1>
+			<h2 class='f4 mid-gray normal'>The new way to track snowfall</h2>
+		</header>
+		<div class='bg-washed-blue'>
+			<div class='pa2 mw8 center'>
+				<h3 class='f5 ttu tracked normal'>Featured</h3>
+			</div>
+		</div>
+		<footer class='bt b--light-gray bg-black-05 pa3 pa6-m pa6-l tc mid-gray'>
+			firstchair.io
+		</footer>
   </main>
 `
