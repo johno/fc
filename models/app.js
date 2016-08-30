55e624d419dd19201490b146b305167819fede9f
John Otander <johnotander@gmail.com>
2016-08-30 11:33:22 -0700

    Shorten up the app model

--- models/app.js
+++ models/app.js
@@ -8,19 +8,11 @@ module.exports = {
   },
 
   effects: {
-    // asynchronous operations that don't modify state directly.
-    // Triggered by actions, can call actions. Signature of (data, state, send, done)
-    /*
-    myEffect: (data, state, send, done) => {
-      // do stuff
-    }
-    */
   },
 
   subscriptions: [
     send => {
-      if (!window.rehydrationState) return
-      const resorts = window.rehydrationState.resorts
+      const resorts = window.rehydrationState && window.rehydrationState.resorts
       send('update', resorts, err => err && console.error(err))
     }
   ]
